b1509dd8cec5ff51fd106011d0b9ffa52ae71911
Marquis <511680713@qq.com>
2020-10-21 22:12:31 +0800

    [update]page

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,6 +5,41 @@ export default {
     'pages/team/index', // 组队
     'pages/me/index' // 我的
   ],
+  subPackages: [
+    {
+      root: 'packageMe',
+      pages: [
+        'pages/bind/index', // 绑定
+        'pages/profile/index', // 个人信息编辑
+        'pages/publish/index', // 我发布的商品
+        'pages/team/index', // 我参与的队伍
+        'pages/trade/index', // 我的交易
+      ]
+    },
+    {
+      root: 'packageSearch',
+      pages: [
+        'pages/idle/index', // 二手闲置搜索
+        'pages/team/index', // 赛事搜索
+      ]
+    },
+    {
+      root: 'packageTeam',
+      pages: [
+        'pages/application/index', // 发起组队申请
+        'pages/detail/index', // 赛事详情
+        'pages/info/index', // 队伍详情
+        'pages/join/index', // 加入队伍申请
+      ]
+    },
+    {
+      root: 'packageIdle',
+      pages: [
+        'pages/application/index', // 发布商品
+        'pages/detail/index', // 闲置详情
+      ]
+    },
+  ],
   window: {
     backgroundTextStyle: 'dark',
     navigationBarBackgroundColor: '#ffb400',
@@ -30,7 +65,7 @@ export default {
       },
       {
         pagePath: 'pages/team/index',
-        text: "组队",
+        text: "赛事",
         iconPath: 'images/team-s.png',
         selectedIconPath: 'images/team.png'
       },
---
+++ src/packageSearch/pages/idle/index.tsx
@@ -0,0 +1,58 @@
+import React, { Component, ComponentClass } from 'react'
+import { connect } from 'react-redux'
+import { View } from '@tarojs/components'
+
+import './index.scss'
+
+// #region 书写注意
+//
+// 目前 typescript 版本还无法在装饰器模式下将 Props 注入到 Taro.Component 中的 props 属性
+// 需要显示声明 connect 的参数类型并通过 interface 的方式指定 Taro.Component 子类的 props
+// 这样才能完成类型检查和 IDE 的自动提示
+// 使用函数模式则无此限制
+// ref: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20796
+//
+// #endregion
+
+type PageStateProps = {}
+
+type PageDispatchProps = {
+  add: () => void
+  dec: () => void
+  asyncAdd: () => any
+}
+
+type PageOwnProps = {}
+
+type PageState = {}
+
+type IProps = PageStateProps & PageDispatchProps & PageOwnProps
+
+interface Index {
+  props: IProps;
+}
+
+@connect(() => ({}), () => ({}))
+class Index extends Component {
+  state = {}
+  componentWillReceiveProps(nextProps) {
+    console.log(this.props, nextProps)
+  }
+
+  componentWillUnmount() { }
+
+  componentDidShow() { }
+
+  componentDidHide() { }
+
+  render() {
+    return (
+      <View className='container'>
+        二手闲置搜索
+      </View>
+    )
+  }
+}
+
+export default Index as ComponentClass<PageOwnProps, PageState>
+
